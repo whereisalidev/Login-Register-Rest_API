29cad9c7aa40b8e4d115ada27634dff8cd017663
ali-jee-786 <786syedalihussain@gmail.com>
2024-07-30 20:21:53 +0500

    login_register_API

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.authentication import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
+
+
+
+
+
