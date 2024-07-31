4a885f3d6b0b7d14ce09e616ae4bbc116ac6b4fe
ali-jee-786 <786syedalihussain@gmail.com>
2024-07-31 07:15:01 +0500

    login_register-REST-API

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -18,34 +18,15 @@ class RegisterAPI(APIView):
     
 
 
-
-
-
-
-
-
-# class RegisterAPI(APIView):
-#     def post(self, request):
-#         data = request.data
-#         serializer = RegisterSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({'message': 'User Registered Successfully'}, status=status.HTTP_201_CREATED)
-#         return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class LoginAPI(APIView):
-#     def post(self, request):
-#         data = request.data
-#         serializer = LoginSerializer(data=data)
-#         if serializer.is_valid():
-#             user = authenticate(username=serializer.data['username'], password = serializer.data['password'])
-#             if not user:
-#                 return Response({'message': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
-#             token, _ = Token.objects.get_or_create(user=user)
-#             return Response({'message': f'User {data['username']} logged in Successfully', 'token': str(token)}, status=status.HTTP_200_OK)
-#         return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
+class LoginAPI(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = LoginSerializer(data=data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+            if user is not None:
+                token, _ = Token.objects.get_or_create(user= user)
+                return Response({'message': 'User Logged in Successfully', 'token': str(token)}, status=status.HTTP_200_OK)
+            return Response({'message': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            
